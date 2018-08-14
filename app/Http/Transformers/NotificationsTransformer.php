d6f3c4ae0b44976f7eb329c32cd70954aebcb997
Anuj Jaha <er.anujjaha@gmail.com>
2018-08-14 17:18:09 +0530

    Added User name

--- app/Http/Transformers/NotificationsTransformer.php
+++ app/Http/Transformers/NotificationsTransformer.php
@@ -30,6 +30,7 @@ class NotificationsTransformer extends Transformer
             "description"       => $item->description,
             "is_read"           => (int) $item->is_read,
             'name'              => $item->user->name,
+            'other_user_name'   => $item->to_user->name,
             'profile_pic'       => isset($item->user->profile_pic) ? URL::to('/').'/uploads/user/' . $item->user->profile_pic : '',
             'created_at'        => Carbon::parse($item->created_at)->diffForHumans(null, true, false) . ' ago'
         ];
