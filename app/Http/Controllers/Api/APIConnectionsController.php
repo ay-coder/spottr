ec8cb2a04905f66429de02dfc3203631a0d3510e
Anuj Jaha <er.anujjaha@gmail.com>
2018-08-22 14:09:36 +0530

    fixed no result

--- app/Http/Controllers/Api/APIConnectionsController.php
+++ app/Http/Controllers/Api/APIConnectionsController.php
@@ -145,8 +145,7 @@ class APIConnectionsController extends BaseApiController
                     return $this->successResponse($itemsOutput);
                 }
 
-                return $this->successResponse('No Result Found !');
-
+                return $this->successResponse(['message' => 'No Result Found !'], 'No Result Found !');
             }
         }
         
